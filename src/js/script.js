fd6dfae948c3ee5c6895a9a9942052e99c18a163
Vladimir <falconv7k@gmail.com>
2021-05-31 19:40:20 +0300

    Catalog section and tabs

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,45 @@ $(document).ready(function () {
       ]
    });
 });
+
+///
+/// Tabs /////////////////////////
+///
+const tabsItems = document.querySelectorAll('.catalog__tab');
+const tabsList = document.querySelector('.catalog__tabs');
+const catalogs = document.querySelectorAll('.catalog__content')
+
+tabsList.addEventListener('click', (e) => {
+   tabsItems.forEach((item, idx) => {
+      if (item === e.target.parentElement) {
+         item.classList.add('catalog__tab_active')
+         catalogs[idx].classList.add('catalog__content_active')
+      } else {
+         item.classList.remove('catalog__tab_active')
+         catalogs[idx].classList.remove('catalog__content_active')
+
+      }
+   });
+});
+
+//
+// Cards ////////////////
+//
+const contentCards = document.querySelectorAll('.catalog-item__content');
+const listCards = document.querySelectorAll('.catalog-item__list');
+
+document.querySelectorAll('.catalog-item__link').forEach((item, idx) => {
+   item.addEventListener('click', (e) => {
+      e.preventDefault();
+      contentCards[idx].classList.remove('catalog-item__content_active');
+      listCards[idx].classList.add('catalog-item__list_active');
+   });
+});
+
+document.querySelectorAll('.catalog-item__back').forEach((item, idx) => {
+   item.addEventListener('click', (e) => {
+      e.preventDefault();
+      listCards[idx].classList.remove('catalog-item__list_active');
+      contentCards[idx].classList.add('catalog-item__content_active');
+   });
+});
