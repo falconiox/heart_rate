9868cdc1eca7c182925229edc81c1afc3523f92e
Vladimir <falconv7k@gmail.com>
2021-06-01 15:09:32 +0300

    delete slick slider, add native js slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,49 @@
-$(document).ready(function () {
-   $('.carousel__inner').slick({
-      autoplay: true,
-      prevArrow: `<button type="button" class="slick-prev">
-                     <img src="icons/left.png" alt="prev" />
-                  </button>`,
-      nextArrow: `<button type="button" class="slick-next">
-                  <img src="icons/right.png" alt="next" />
-               </button>`,
-      responsive: [
-         {
-            breakpoint: 992,
-            settings: {
-               dots: true,
-               arrows: false
-            }
-         }
-      ]
-   });
+/// Slider
+
+const prevBtn = document.querySelector('.carousel__prev');
+const nextBtn = document.querySelector('.carousel__next');
+let count = 0;
+let value = 0;
+
+nextBtn.addEventListener('click', () => {
+   value -= 750;
+
+   if (value <= -2250) {
+      value = 0;
+   }
+
+   document.querySelector(
+      '.carousel__slides'
+   ).style.transform = `translateX(${value}px)`;
 });
+
+prevBtn.addEventListener('click', () => {
+   value += 750
+   if(value > 0) {
+      value = -1500
+   }
+   console.log(value);
+   document.querySelector(
+      '.carousel__slides'
+   ).style.transform = `translateX(${value}px)`;
+});
+
 
 ///
 /// Tabs /////////////////////////
 ///
 const tabsItems = document.querySelectorAll('.catalog__tab');
 const tabsList = document.querySelector('.catalog__tabs');
-const catalogs = document.querySelectorAll('.catalog__content')
+const catalogs = document.querySelectorAll('.catalog__content');
 
 tabsList.addEventListener('click', (e) => {
    tabsItems.forEach((item, idx) => {
       if (item === e.target.parentElement) {
-         item.classList.add('catalog__tab_active')
-         catalogs[idx].classList.add('catalog__content_active')
+         item.classList.add('catalog__tab_active');
+         catalogs[idx].classList.add('catalog__content_active');
       } else {
-         item.classList.remove('catalog__tab_active')
-         catalogs[idx].classList.remove('catalog__content_active')
-
+         item.classList.remove('catalog__tab_active');
+         catalogs[idx].classList.remove('catalog__content_active');
       }
    });
 });
